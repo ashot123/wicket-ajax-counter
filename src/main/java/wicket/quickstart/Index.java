49918d965a1f3bf3afee57e0ca331729cb572ec7
Ashot K <ashot2005@yahoo.com>
2014-09-07 18:29:40 +0400

    Some descriptions.

--- src/main/java/wicket/quickstart/Index.java
+++ src/main/java/wicket/quickstart/Index.java
@@ -9,9 +9,10 @@ import org.apache.wicket.markup.html.basic.Label;
 
 public class Index extends WebPage {
     public Index() {
+        // Model subclass that increases its counter every time the getObject method gets called,
+        // and returns its value. For example when clicking CTRL+F5 onClick() not called
         Model<Integer> model = new Model<Integer>() {
             private int counter = 0;
-
 
             @Override
             public Integer getObject() {
